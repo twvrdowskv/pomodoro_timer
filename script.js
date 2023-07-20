4b6293af044f822e85a47426446c46e0c049789a
twvrdowskv <ktwardowska9@wp.pl>
2023-07-20 21:31:09 +0200

    commit

---
+++ script.js
@@ -0,0 +1,60 @@
+let workTittle = document.getElementById('work');
+let breakTittle = document.getElementById('break');
+
+let workTime = 25;
+let breakTime = 5;
+
+let seconds = "00";
+
+//display
+window.onload = () => {
+    document.getElementById('minutes').innerHTML = workTime;
+    document.getElementById('seconds').innerHTML = seconds;
+
+    workTittle.classList.add('active');
+}
+
+//start timer
+function start() {
+    //change button
+    document.getElementById('start').style.display = "none";
+    document.getElementById('reset').style.display = "block";
+
+    seconds = 59;
+    let workMinutes = workTime - 1;
+    let breakMinutes = breakTime - 1;
+
+    breakCount = 0;
+
+    let timerFunction = () => {
+        document.getElementById('minutes').innerHTML = workMinutes;
+        document.getElementById('seconds').innerHTML = seconds;
+
+        seconds = seconds - 1;
+
+        if(seconds === 0) {
+            workMinutes = workMinutes - 1;
+            if(workMinutes === -1){
+                if(breakCount % 2 === 0){
+                    workMinutes = breakMinutes;
+                    breakCount++;
+
+                    //change the panel
+                    workTittle.classList.remove('active');
+                    breakTittle.classList.add('active');
+                }
+                else{
+                    workMinutes = workTime;
+                    breakCount++;
+
+                    //change the panel
+                    workTittle.classList.add('active');
+                    breakTittle.classList.remove('active');
+                }
+            }
+            seconds = 59;
+        }
+    }
+    setInterval(timerFunction, 1000); //1000=1s
+}
+
